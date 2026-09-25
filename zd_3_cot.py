496c06563b0967d9f3ab4acbe4ef9dea2a645d3a
daimontel <sashs.06@mail.ru>
2026-09-25 11:25:16 +0000

    синхрон пропустил 3

---
+++ zd_3_cot.py
@@ -0,0 +1,21 @@
+import math
+
+a = -5.0
+b = 5.0
+step = 0.5
+
+x = a
+
+print(f"{'x':>8} | {'y':>12}")
+print("-" * 23)
+
+while x <= b + 1e-9:
+    # Составная функция
+    if x < 0:
+        y = x**2 + 2*x
+    else:
+        y = math.sqrt(x) + 1
+    
+    print(f"{x:8.2f} | {y:12.5f}")
+
+    x += step
